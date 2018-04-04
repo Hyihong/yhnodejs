b5a0172a50b70392b3527f223fb48e41d912ed1f
chenyihong <496695119@qq.com>
2018-04-04 10:10:40 +0800

    数据库初始化操作

--- db/script/initDB.js
+++ db/script/initDB.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path')
 const mysql = require('mysql')
 
-const sql = fs.readFileSync( path.resolve(__dirname,'../sql/init.sql'), 'utf8' );
+const sqlList = fs.readFileSync( path.resolve(__dirname,'../sql/init.sql'), 'utf8' );
 
 
 const connection = mysql.createConnection({
@@ -14,7 +14,14 @@ const connection = mysql.createConnection({
 })
 
 
-connection.query( sql , (error, results, fields) => {
-    if (error) throw error ;
-    console.log( results )
-});
+sqlList.split(";").map( sql => {
+    //最后一条为回车换行
+    if(sql !== '\r\n'){
+        connection.query( sql , (error, results, fields) => {
+            console.log(sql)
+            if (error) throw error ;
+            console.log("执行完毕！")
+        });
+    }
+  
+})
--- db/sql/init.sql
+++ db/sql/init.sql
@@ -1,8 +1,12 @@
 
-CREATE TABLE IF NOT EXISTS `article`(
-   `article_id` INT UNSIGNED AUTO_INCREMENT,
-   `article_title` TEXT NOT NULL,
-   `article_author` VARCHAR(40) NOT NULL,
-   `submission_date` DATE,
-   PRIMARY KEY ( `article_id` )
+CREATE TABLE IF NOT EXISTS `user`(
+   `user_id` INT UNSIGNED AUTO_INCREMENT,
+   `user_name` VARCHAR(40) NOT NULL,
+   `user_password` VARCHAR(40) NOT NULL,
+   PRIMARY KEY ( `user_id` )
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 插入数据
+INSERT INTO `user` (user_id,user_name,user_password) VALUES( 1,'Hyihong','aptx4869' );
+
+INSERT INTO `user` (user_id,user_name,user_password) VALUES( 2,'Wbaichao','aptx4869' );
